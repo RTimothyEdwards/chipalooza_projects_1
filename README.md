21b2a279b8e988221ff6494e4fbb1a57a1e1097f
Tim Edwards <tim@opencircuitdesign.com>
2024-04-26 16:30:57 -0400

    Update after running precheck locally and fixing all outstanding errors.

--- README.md
+++ README.md
@@ -7,14 +7,83 @@
 | :exclamation: Important Note            |
 |-----------------------------------------|
 
-## Please fill in your project documentation in this README.md file 
-
 This is the first test chip from the Chipalooza challenge 2024.
 11 designs made it on the April tapeout.
-(Documentation to be completed)
 
+The projects on this tapeout and their source URLs are as follows:
 
-:warning: | Use this sample project for analog user projects. 
+	1.  	High-speed crystal oscillator (4-16MHz)
+		hsxo-cpz1 
+		https://github.com/htfab/hsxo-cpz1
+		Tamas Hubai
+
+	2.  	Brownout detector
+		sky130_ajc_ip__brownout
+		https://github.com/ajcci/sky130_ajc_ip__brownout
+		Robin Tsang
+
+	3.  	Overvoltage detector
+		sky130_ajc_ip__overvoltage
+		https://github.com/ajcci/sky130_ajc_ip__overvoltage
+		Robin Tsang
+
+	4.  	Power-on-Reset
+		sky130_ajc_ip__por
+		https://github.com/ajcci/sky130_ajc_ip__por
+		Robin Tsang
+
+	5.  	Comparator
+		sky130_ak_ip__comparator
+		https://github.com/andrewkkang/sky130_ak_ip__comparator
+		Andrew Kang
+
+	6.  	Low-speed crystal oscillator (32kHz)
+		sky130_be_ip__lsxo
+		https://github.com/b-etz/sky130_be_ip__lsxo
+		Brady Etz
+
+	7.  	Temperature sensor
+		sky130_od_ip__tempsensor
+		https://github.com/ordicker/sky130_od_ip__tempsensor
+		Or Dicker
+
+	8.  	Low power operational amplifier
+		sky130_rodovalho_ip__lpopamp
+		https://github.com/lhrodovalho/sky130_rodovalho_ip__lpopamp
+		Luis Henrique Rodovalho
+
+	9.  	High gain-bandwidth operational amplifier
+		sky130_td_ip__opamp_hp
+		https://github.com/tdextrous/sky130_td_ip__opamp_hp
+		Thomas Dexter
+
+	10. 	Overvoltage detector
+		sky130_vbl_ip__overvoltage
+		https://github.com/LDFranck/sky130_vbl_ip__overvoltage
+		Lucas Franck, William Orlato, and Toni Tejada
+
+	11.	Bandgap (not from Chipalooza)
+		bandgap
+		https://github.com/christoph-weiser/mpw7
+		Christoph Weiser
+
+	12.	Power FET (not from Chipalooza)
+		power_stage
+		(in this repository)
+		Weston Braun
+
+	13.	Bias current generator (not from Chipalooza)
+		bias_generator
+		(in this repository)
+		Tim Edwards
+
+	14.	Ground-isolated analog switch (not from Chipalooza)
+		isolated_switch_ena1v8
+		(in this repository)
+		Tim Edwards
+
+(Documentation, including test instructions, still needs to be completed)
+
 :---: | :---
 
 ---
--- mag/construct_testchip.tcl
+++ mag/construct_testchip.tcl
@@ -1,3 +1,19 @@
+# SPDX-FileCopyrightText: 2020 Efabless Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
 # Construct the test chip
 
 if {[catch {set PDK_ROOT $::env(PDK_ROOT)}]} {
@@ -7,49 +23,40 @@ if {[catch {set PDK $::env(PDK)}]} {
     set PDK sky130A
 }
 
+# The remainder will come from .mag (unless I create individual GDS files)
+
+addpath ../../chipalooza/hsxo-cpz1/mag
+# addpath ../../chipalooza/sky130_ak_ip__comparator/mag
+addpath ../../chipalooza/sky130_od_ip__tempsensor/mag
+addpath ../../chipalooza/sky130_td_ip__opamp_hp/mag
+addpath ../../chipalooza/sky130_vbl_ip__overvoltage/mag
+addpath ../../chipalooza/sky130_be_ip__lsxo/mag
+addpath ../../chipalooza/sky130_rodovalho_ip__lpopamp/mag
+
+# These have been modified to remove same names of non-matching circuits:
+# comparator, ibias_gen, rc_osc, rstring_mux, schmitt_trigger
+addpath ../../chipalooza/sky130_ajc_ip__brownout/mag
+addpath ../../chipalooza/sky130_ajc_ip__por/mag
+addpath ../../chipalooza/sky130_ajc_ip__overvoltage/mag
+
 # Pull the cells which have GDS
 # gds readonly true
 # gds rescale false
 # gds drccheck off
 
-# Pre-read the standard cell GDS
-gds read $PDK_ROOT/$PDK/libs.ref/sky130_fd_sc_hd/gds/sky130_fd_sc_hd.gds
-gds read $PDK_ROOT/$PDK/libs.ref/sky130_fd_sc_hvl/gds/sky130_fd_sc_hvl.gds
-
 # Force the standard cell GDS just read to take precedence
 gds noduplicates true
 
-# These have cells of the same name with different contents, so enforce unique names
-gds unique true
-gds read ../../chipalooza/sky130_ajc_ip__brownout/gds/sky130_ajc_ip__brownout.gds
-gds read ../../chipalooza/sky130_ajc_ip__por/gds/sky130_ajc_ip__por.gds
-gds read ../../chipalooza/sky130_ajc_ip__overvoltage/gds/sky130_ajc_ip__overvoltage.gds
-gds unique false
-
-# Non-chipalooza items
+# Non-chipalooza items, GDS only
 gds read ../../weiser_analog_mpw7/gds/bandgap.gds
 gds read ../../weiser_analog_mpw7/gds/bias_basis_current.gds
 
-# For the rest, there are no known name collisions.
-gds read ../../chipalooza/sky130_be_ip__lsxo/gds/sky130_be_ip__lsxo.gds
-gds read ../../chipalooza/sky130_rodovalho_ip__lpopamp/gds/lpopamp.gds
-# gds read ../../chipalooza/sky130_ac3e_ip__temp_sdc/modules/SDC_DIGITAL/layout/SDC_DIGITAL.gds
+gds noduplicates false
 
-# Not from .mag . . .
-# addpath ../../chipalooza/sky130_ac3e_ip__temp_sdc/modules
-# addpath ../../chipalooza/sky130_ajc_ip__brownout/mag
-# addpath ../../chipalooza/sky130_ajc_ip__overvoltage/mag
-# addpath ../../chipalooza/sky130_ajc_ip__por/mag
-# addpath ../../chipalooza/sky130_be_ip__lsxo/mag
-# addpath ../../chipalooza/sky130_rodovalho_ip__lpopamp/mag
-
-# The remainder will come from .mag (unless I create individual GDS files)
-
-addpath ../../chipalooza/hsxo-cpz1/mag
-addpath ../../chipalooza/sky130_ak_ip__comparator/mag
-addpath ../../chipalooza/sky130_od_ip__tempsensor/mag
-addpath ../../chipalooza/sky130_td_ip__opamp_hp/mag
-addpath ../../chipalooza/sky130_vbl_ip__overvoltage/mag
+# Test---Read project from GDS as read-only
+gds readonly true
+gds rescale false
+gds read ../../chipalooza/sky130_ak_ip__comparator/gds/sky130_ak_ip__comparator.gds.gz
 
 # Load the project wrapper cell
 load user_analog_project_wrapper
